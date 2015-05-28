e845858072baa6fb8f57720f1d8ed9bd69f72c00
Andrew LaBombarda <alabomb@gmail.com>
2015-05-28 13:58:47 -0500

    Changed input requirement, added sample output
    Originally had the program only accept prime numbers as an input, which
    after a re-read of the prompt isn't actually a requirement. Added sample
    outputs using examples in the prompt.

--- FindNextPrime.cpp
+++ FindNextPrime.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
 
-int findNextPrime(int n);   //given a prime number n, find the next closest prime number above n
+int findNextPrime(int n);   //given a number n, find the next closest prime number above n
 bool isPrime(int n);        //given a number n, determine if it is prime
 
 int main()
 {
-    bool goodInput = false;
     int input;
 
-    //make sure we're getting prime numbers
-    while (!goodInput)
-    {
-        std::cout << "Please enter a prime number: ";
-        std::cin >> input;
-        if (isPrime(input))     //carry on, nothing to see here
-            goodInput = true;
-        else                    //try again
-            std::cout << input << " is not a prime number. \n";
-    }
+    std::cout << "Please enter a prime number: ";
+    std::cin >> input;
 
     std::cout << "\nThe next prime number after " << input << " is " << findNextPrime(input) << ". \n";
 }
 
-//given a prime number n, find the next closest prime number above n
+//given a number n, find the next closest prime number above n
 int findNextPrime(int n)
 {
     int nextPrime = n;
@@ -51,3 +42,23 @@ bool isPrime(int n)
 
     return true;
 }
+
+/*
+SAMPLE OUTPUT
+
+Please enter a prime number: 6
+
+The next prime number after 6 is 7.
+
+Please enter a prime number: 10
+
+The next prime number after 10 is 11.
+
+Please enter a prime number: 11
+
+The next prime number after 11 is 13.
+
+Please enter a prime number: 788
+
+The next prime number after 788 is 797.
+*/
